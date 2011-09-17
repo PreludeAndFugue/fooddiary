9718812f2ceac6e6ccec210e1339645daf02212d
Gary Kerr <gdrummondk@gmail.com>
2011-09-17 02:28:49 +0100

    Status bar startup
    The browser overlay now uses the status bar. The text 'Food Diary' is
    added to the status bar. When clicked on of the following will occur:
    
    Food diary closed -> will be opened
    Food diary opened and has focus -> will be closed
    Food diary opened without focus -> tab will get focus

--- chrome/content/browserOverlay.js
+++ chrome/content/browserOverlay.js
@@ -1,12 +1,26 @@
 if (typeof(fooddiary) == 'undefined')
-    var fooddiary = {};
+    var fooddiary = {
+        tab: null,
+    };
 
 /*******************************************************************************
  * called when the 'Food Diary' menu button in the Tools menu is clicked. Opens
  * the XUL interface in a new tab of the browser.
  ******************************************************************************/
-fooddiary.start = function(e)
-{
-    var tab = gBrowser.addTab("chrome://fooddiary/content/fooddiary.xul");
-    gBrowser.selectedTab = tab;
+fooddiary.start = function(e) {
+    if (this.tab === null) {
+        this.tab = gBrowser.addTab("chrome://fooddiary/content/fooddiary.xul");
+        gBrowser.selectedTab = this.tab;
+    } else {
+        /*
+        if (gBrowser.selectedTab != this.tab) {
+            gBrowser.selectedTab = this.tab;
+        */
+        if (gBrowser.selectedTab != this.tab) {
+            gBrowser.selectedTab = this.tab;
+        } else {
+            gBrowser.removeTab(this.tab);
+            this.tab = null;
+        }
+    }
 }
