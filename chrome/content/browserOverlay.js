a6caf852254f54c6b2cbd93816ec57b00f17844e
Gary Kerr <gdrummondk@gmail.com>
2011-09-18 16:17:40 +0100

    Tab close event handler
    Event handler for clicking on the close button on the food diary
    tab.

--- chrome/content/browserOverlay.js
+++ chrome/content/browserOverlay.js
@@ -1,6 +1,8 @@
 if (typeof(fooddiary) == 'undefined')
     var fooddiary = {
         tab: null,
+        container: null,
+        shut: null,
     };
 
 /*******************************************************************************
@@ -8,19 +10,30 @@ if (typeof(fooddiary) == 'undefined')
  * the XUL interface in a new tab of the browser.
  ******************************************************************************/
 fooddiary.start = function(e) {
+    if (this.container === null) {
+        this.container = gBrowser.tabContainer;
+    }
+
+    if (this.shut === null) {
+        this_fd = this;
+        this.shut = function(event) {
+            if (this_fd.tab === event.target) {
+                this_fd.container.removeEventListener('TabClose', this_fd.shut, false);
+                this_fd.tab = null;
+            }
+        };
+    }
+
     if (this.tab === null) {
         this.tab = gBrowser.addTab("chrome://fooddiary/content/fooddiary.xul");
         gBrowser.selectedTab = this.tab;
+        this.container.addEventListener('TabClose', this.shut, false);
+
     } else {
-        /*
-        if (gBrowser.selectedTab != this.tab) {
-            gBrowser.selectedTab = this.tab;
-        */
         if (gBrowser.selectedTab != this.tab) {
             gBrowser.selectedTab = this.tab;
         } else {
             gBrowser.removeTab(this.tab);
-            this.tab = null;
         }
     }
-}
+};
